327b065623eebf58545e988d3bed8a3bf2f6e5ff
Doghouse-Mike <90314813+Doghouse-Mike@users.noreply.github.com>
2025-04-02 23:44:29 +0100

    Update quartz.layout.ts

--- quartz.layout.ts
+++ quartz.layout.ts
@@ -5,25 +5,7 @@ import * as Component from "./quartz/components"
 export const sharedPageComponents: SharedLayout = {
   head: Component.Head(),
   header: [],
-  afterBody: [
-    `
-    <script async crossorigin="anonymous"
-      src="https://giscus.app/client.js"
-      data-repo="Doghouse-Mike/my-notes"
-      data-repo-id="R_kgDOOPVAfw"
-      data-category="Announcements"
-      data-category-id="DIC_kwDOOPVAf84CouAn"
-      data-mapping="pathname"
-      data-strict="0"
-      data-reactions-enabled="1"
-      data-emit-metadata="0"
-      data-input-position="top"
-      data-theme="catppuccin_mocha"
-      data-lang="en"
-      data-loading="lazy">
-    </script>
-    `,
-  ],
+  afterBody: [],
   footer: Component.Footer({
     links: {
       GitHub: "https://github.com/jackyzha0/quartz",
@@ -57,11 +39,11 @@ export const defaultContentPageLayout: PageLayout = {
     Component.Graph(),
     Component.DesktopOnly(Component.TableOfContents()),
     Component.Backlinks(),
-    Component.RecentNotes(),
+    Component.RecentNotes()
   ],
 }
 
-// components for pages that display lists of pages (e.g. tags or folders)
+// components for pages that display lists of pages  (e.g. tags or folders)
 export const defaultListPageLayout: PageLayout = {
   beforeBody: [Component.Breadcrumbs(), Component.ArticleTitle(), Component.ContentMeta()],
   left: [
