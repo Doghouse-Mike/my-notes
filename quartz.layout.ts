b75dc9174ee007858c414d1c4469c90a7cd35d5a
Doghouse-Mike <90314813+Doghouse-Mike@users.noreply.github.com>
2025-04-02 23:23:07 +0100

    Update quartz.layout.ts
    *should* enable giscus without borking anything

--- quartz.layout.ts
+++ quartz.layout.ts
@@ -5,7 +5,29 @@ import * as Component from "./quartz/components"
 export const sharedPageComponents: SharedLayout = {
   head: Component.Head(),
   header: [],
-  afterBody: [],
+  afterBody: [
+    {
+      Component: () => (
+        <script
+          src="https://giscus.app/client.js"
+          data-repo="Doghouse-Mike/my-notes"
+          data-repo-id="R_kgDOOPVAfw"
+          data-category="Announcements"
+          data-category-id="DIC_kwDOOPVAf84CouAn"
+          data-mapping="pathname"
+          data-strict="0"
+          data-reactions-enabled="1"
+          data-emit-metadata="0"
+          data-input-position="top"
+          data-theme="catppuccin_mocha"
+          data-lang="en"
+          data-loading="lazy"
+          crossOrigin="anonymous"
+          async
+        ></script>
+      ),
+    },
+  ],
   footer: Component.Footer({
     links: {
       GitHub: "https://github.com/jackyzha0/quartz",
@@ -39,11 +61,11 @@ export const defaultContentPageLayout: PageLayout = {
     Component.Graph(),
     Component.DesktopOnly(Component.TableOfContents()),
     Component.Backlinks(),
-    Component.RecentNotes()
+    Component.RecentNotes(),
   ],
 }
 
-// components for pages that display lists of pages  (e.g. tags or folders)
+// components for pages that display lists of pages (e.g. tags or folders)
 export const defaultListPageLayout: PageLayout = {
   beforeBody: [Component.Breadcrumbs(), Component.ArticleTitle(), Component.ContentMeta()],
   left: [
