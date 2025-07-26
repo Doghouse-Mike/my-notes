a6a612c389a25b59891b7d382f5d86ae2502586b
Doghouse-Mike <90314813+Doghouse-Mike@users.noreply.github.com>
2025-07-26 13:55:09 +0100

    Update quartz.config.ts

--- quartz.config.ts
+++ quartz.config.ts
@@ -16,7 +16,7 @@ const config: QuartzConfig = {
       provider: "plausible",
     },
     locale: "en-US",
-    baseUrl: "28mm.coffee/",
+    baseUrl: "28mm.coffee",
     ignorePatterns: ["private", "templates", ".obsidian"],
     defaultDateType: "created",
     theme: {
