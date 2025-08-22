ae8bf531238f814a959d431650af9521f355cc28
Doghouse-Mike <90314813+Doghouse-Mike@users.noreply.github.com>
2025-08-22 21:31:27 +0100

    Update Explorer.tsx

--- quartz/components/Explorer.tsx
+++ quartz/components/Explorer.tsx
@@ -29,14 +29,15 @@ const defaultOptions: Options = {
   mapFn: (node) => {
     return node
   },
-  sortFn: (a, b) => {
-    // Sort order: folders first, then files. Sort folders and files alphabeticall
-    if ((!a.isFolder && !b.isFolder) || (a.isFolder && b.isFolder)) {
-      // numeric: true: Whether numeric collation should be used, such that "1" < "2" < "10"
-      // sensitivity: "base": Only strings that differ in base letters compare as unequal. Examples: a ≠ b, a = á, a = A
-      return a.displayName.localeCompare(b.displayName, undefined, {
-        numeric: true,
-        sensitivity: "base",
+  sortFn: (a: FileNode, b: FileNode) => {
+    const dateA = a.file?.frontmatter?.["created"]
+      ? new Date(a.file.frontmatter["created"])
+      : new Date(0);
+    const dateB = b.file?.frontmatter?.["created"]
+      ? new Date(b.file.frontmatter["created"])
+      : new Date(0);
+    return dateB.getTime() - dateA.getTime(); 
+  },
       })
     }
 
