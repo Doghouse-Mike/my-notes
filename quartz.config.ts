a559ebf31ecfea31d1ec0017904ad809764a74d5
Doghouse-Mike <90314813+Doghouse-Mike@users.noreply.github.com>
2026-04-11 21:55:37 +0100

    Update quartz.config.ts
    Font swap

--- quartz.config.ts
+++ quartz.config.ts
@@ -24,8 +24,8 @@ const config: QuartzConfig = {
       cdnCaching: true,
       typography: {
         header: "VT323",
-        body: "JetBrains Mono",
-        code: "Share Tech Mono",
+        body: "Atkinson Hyperlegible Next",
+        code: "JetBrains Mono",
       },
       colors: {
         lightMode: {
